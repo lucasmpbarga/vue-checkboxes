e133682222ecac861e822fbd7751518f27eed661
Lucas Barga <lucasmpbarga@gmail.com>
2022-08-02 01:27:03 -0300

    up up up

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <Checkbox
+      v-on:check-event="checkOption"
+      id="green"
+      name="green"
+      :isChecked="valueIsChecked('green')"
+    />
+    <Checkbox
+      v-on:check-event="checkOption"
+      id="red"
+      name="red"
+      :isChecked="valueIsChecked('red')"
+    />
+    <Checkbox
+      v-on:check-event="checkOption"
+      id="blue"
+      name="blue"
+      :isChecked="valueIsChecked('blue')"
+    />
+    <Checkbox
+      v-on:check-event="checkOption"
+      id="yellow"
+      name="yellow"
+      :isChecked="valueIsChecked('yellow')"
+    />
+    <Checkbox
+      v-on:check-event="checkOption"
+      id="gray"
+      name="gray"
+      :isChecked="valueIsChecked('gray')"
+    />
+    <button @click="printCheckedValues">print my checked values</button>
+  </div>
+</template>
+
+<script>
+import Checkbox from "./components/Checkbox.vue";
+import "vue-swatches/dist/vue-swatches.css";
+
+export default {
+  name: "App",
+  components: {
+    Checkbox,
+  },
+  data: () => ({
+    checkeds: [],
+  }),
+  methods: {
+    checkOption(option) {
+      const optionInArray = this.valueIsChecked(option);
+
+      if (optionInArray) {
+        const index = this.checkeds.indexOf(option);
+
+        this.checkeds.splice(index, 1);
+      } else {
+        this.checkeds.push(option);
+      }
+    },
+    valueIsChecked(value) {
+      return this.checkeds.includes(value);
+    },
+    printCheckedValues() {
+      console.log(this.checkeds);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Checkbox.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :id="id"
+      :name="name"
+      :checked="isChecked"
+      @click="$emit('check-event', id)"
+    />
+    <label for="scales">{{ this.$props.name }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    name: String,
+    isChecked: Boolean,
+  },
+  mounted() {
+    console.log("props", this.$props);
+  },
+  methods: {
+    print() {
+      console.log("props", this.$props);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.swatcher_item {
+  background: #eee;
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  margin: 4px;
+  border-radius: 60px;
+  position: relative;
+  cursor: pointer;
+}
+.swatcher_item:hover,
+.swatcher_item.active {
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.9);
+}
+.swatcher_item.active svg {
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 20px;
+  height: 20px;
+}
+</style>
